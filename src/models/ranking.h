cf3bbd55f1909d6ccdf39c0e2812162713625ab6
ckob <ckobestudos@gmail.com>
2021-10-13 16:24:46 -0300

    Funçao que valida nova pontuaçao no ranking

--- src/models/ranking.h
+++ src/models/ranking.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include "../core.h"
+//#include "../core.h"
 
 struct points{
     char name[30];
@@ -9,7 +9,7 @@ struct points{
 };
 
 
-struct ranking{
+struct ranking{ 
     struct points *first;
     struct points *second;
     struct points *third;
@@ -17,6 +17,7 @@ struct ranking{
     struct points *fifth;
 };
 
+int goToRank(struct points *self);
 
 void pontuacaoSuficiente(struct points *self);
 
@@ -28,6 +29,5 @@ void saveRank(struct ranking *self);
 // Funçao que retorna um struct ranking, carregando ele da memoria. (Lembre-se que podem não haver pontuações nele)
 // Função que retorna um char* do ranking (Use a função à cima e converta em texto)
 // Função que salva um ranking na memoria
-// Função que dado uma pontuacao em int e um ranking retorna se deve adicionar ele ao ranking ou não
 // Função que adiciona um rank à um ranking, na posição correta e remove o que sobrar, salvando na memoria.
 
